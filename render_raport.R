1e86179e6bfc2c0d35e859188074652798edf947
MichalB888 <biesiadv2@gmail.com>
2026-01-22 19:25:45 +0100

    Add statistical test tables to section 5, add section 5.6 (addiction impact on academic performance by age), add render script

---
+++ render_raport.R
@@ -0,0 +1,3 @@
+# Skrypt do wygenerowania raportu HTML
+setwd("c:/Users/biesi/Desktop/studia/Analiza danych w R/proba")
+rmarkdown::render("Raport końcowy/Raport - Uzależnienie od mediów społecznościowych.rmd")
